6258f905914f7eda7f3765977da18f2c9c576501
kamel hloubi <hloubi81@gmail.com>
2026-06-09 00:15:33 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,11 @@ const AuthenticatedApp = () => {
       return <UserNotRegisteredError />;
     } else if (authError.type === 'auth_required') {
       navigateToLogin();
-      return null;
+      return (
+        <div className="fixed inset-0 flex items-center justify-center">
+          <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-800 rounded-full animate-spin"></div>
+        </div>
+      );
     }
   }
 
